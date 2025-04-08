9d80ed536eb11b25085440992ce0cc1a6db57996
polamounir103 <polamounir103@gmail.com>
2025-04-08 17:14:12 +0200

    first commit

--- src/services/signalr.js
+++ src/services/signalr.js
@@ -1,15 +1,20 @@
 import * as signalR from "@microsoft/signalr";
 
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxNmFmMWUyNy02MTJhLTQ3NzMtOWQzOS0wOGRkNmQwOWY2Y2MiLCJGdWxsTmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbkBncmFkZWNvbS5jb20iLCJVc2VyVHlwZSI6IkFkbWluIiwibmJmIjoxNzQ0MTIyOTcxLCJleHAiOjE4MDg5MjI5NzEsImlhdCI6MTc0NDEyMjk3MX0.KIAff3HOZrPQz3DgC1owUCN2Z4uFJtShZvbhVVsuXiw";
+localStorage.setItem(
+  "token",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxNmFmMWUyNy02MTJhLTQ3NzMtOWQzOS0wOGRkNmQwOWY2Y2MiLCJGdWxsTmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbkBncmFkZWNvbS5jb20iLCJVc2VyVHlwZSI6IkFkbWluIiwibmJmIjoxNzQ0MTIyOTcxLCJleHAiOjE4MDg5MjI5NzEsImlhdCI6MTc0NDEyMjk3MX0.KIAff3HOZrPQz3DgC1owUCN2Z4uFJtShZvbhVVsuXiw"
+);
+const token = localStorage.getItem("token");
+
 
 let connection = null;
-
+localStorage;
 export const startConnection = () => {
   if (connection && connection.state === signalR.HubConnectionState.Connected) {
-    return Promise.resolve(connection); 
+    return Promise.resolve(connection);
   }
 
+  
   connection = new signalR.HubConnectionBuilder()
     .withUrl("https://zerobyte.localto.net/hubs/chat", {
       accessTokenFactory: () => token,
